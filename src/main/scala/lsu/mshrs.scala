ea00a08ae7000cb7b4da687662daead4e0e9c4d7
godjunsik <bbohae@snu.ac.kr>
2020-10-29 05:54:40 +0000

    revise commit_line := ture  when br_mask != 0

--- src/main/scala/lsu/mshrs.scala
+++ src/main/scala/lsu/mshrs.scala
@@ -266,8 +266,18 @@ class BoomMSHR(implicit edge: TLEdgeOut, p: Parameters) extends BoomModule()(p)
     io.resp.bits.data := loadgen.data
     io.resp.bits.is_hella := rpq.io.deq.bits.is_hella
     when (rpq.io.deq.fire()) {
-      when(io.req.uop.br_mask){
+      /*
+      when(!io.brupdate.b2.mispredict){
         commit_line := false.B
+      }
+      */
+      when(io.req.bits.uop.br_mask){
+        when(io.brupdate.b2.mispredict){
+          commit_line := false.B
+        }
+        otherwise{
+          commit_line := true.B
+        }
       }
       .otherwise{ 
         commit_line   := true.B
